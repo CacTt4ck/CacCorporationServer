7cdbb1f1d86ebf15262bd8831bc2a84b66a8a971
Thomas C <lachainedelightning13090@gmail.com>
2025-03-24 20:26:40 +0100

    Fix TU

--- src/test/java/com/tcaputi/back/caccorporationserver/CacCorporationServerApplicationTests.java
+++ src/test/java/com/tcaputi/back/caccorporationserver/CacCorporationServerApplicationTests.java
@@ -1,9 +1,7 @@
 package com.tcaputi.back.caccorporationserver;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CacCorporationServerApplicationTests {
 
 	@Test
